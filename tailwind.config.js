c45612877330de656d7c7378210aa6a1e3ea8f3f
GrandeMatheus <matheusgrande91@hotmail.comgit>
2025-04-13 11:49:15 -0300

    ✨ Initial commit - site da Dra. Giovanna Paulo

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          pink: "#E5A9A9",
+          mint: "#A6D1B9",
+          moss: "#83A79D",
+        },
+      },
+      fontFamily: {
+        syncopate: ['"Syncopate"', 'sans-serif'],
+        outfit: ['"Outfit"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
