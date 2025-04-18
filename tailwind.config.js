f8c62047034229e3e3bf828d82c10e15d0be361d
Matheus <matheusgrande91@hotmail.com>
2025-04-17 22:30:03 -0300

    feat: finalização da Home com animações AOS, novo texto de confiança para atendimento, CTA aprimorado e integração com Instagram via Elfsight

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ export default {
     extend: {
       colors: {
         brand: {
-         
           pink: "#E5A9A9",
           mint: "#A6D1B9",
           moss: "#83A79D",
+          backgroundStart: "#FFF8F6",
+          backgroundEnd: "#E3F4EF",
         },
+      },
+      backgroundImage: {
+        'brand-gradient': 'linear-gradient(to bottom right, #FFF8F6, #E3F4EF)',
       },
       fontFamily: {
         syncopate: ['"Syncopate"', 'sans-serif'],
@@ -18,5 +22,7 @@ export default {
   },
   plugins: [],
 };
+
+
 
 
